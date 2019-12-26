6ae4d589f1f208b7a11cccf2677de09b6dd12351
aethermc <59040078+aethermc@users.noreply.github.com>
2019-12-26 09:57:06 +0800

    Update crnv2.js

--- crnv2.js
+++ crnv2.js
@@ -2,7 +2,7 @@ function semak(nokp){
 	var ajaxRequest;  // The variable that makes Ajax possible!
   	//alert(nokp);
 	if(nokp==""){
-		alert("Sila masukkan No. Kad Pengenalan / Sijil Lahir");
+		alert("Sila masukkandawdwad No. Kad Pengenalan / Sijil Lahir");
 		document.getElementById("txtIC").focus();
 		return false;
 	}
